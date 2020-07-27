434167353e27d4b059391ae852a704d46f9028de
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 16:40:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
       for (let i =0; i< cart.length -1; i++){
         output = `${output} ${cart[i].itemName} at $${cart[i].itemPrice}, `
       } //for 
-      return
+      return `${output}
     } //else
 }
 
