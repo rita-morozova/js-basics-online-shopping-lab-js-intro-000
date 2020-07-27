65b7072074b312dcee450b813d3161be97707307
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 16:30:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function viewCart() {
   if(cart.length ===0){
     return "Your shopping cart is empty."
   }//if 
-   else if (getCart.length ===1){
-    return `In your cart, you have ${getCart[0].itemName} at ${getCart[0].itemPrice}.`
+   else if (cart.length ===1){
+    return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
    } //else if 
   else {
     for (let i=0; i < getCart.length; i++){
