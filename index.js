36f5111d1c4cdbf36874ff40a51a158f520d934c
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 16:52:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 
 function removeFromCart(item) {
  for (let i =0; i< cart.length; i++){
-   if (cart[i].itemName != item){
+   if (cart[item] != item){
      return "That item is not in your cart."
    }//if
  }//for
